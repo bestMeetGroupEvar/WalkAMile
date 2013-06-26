39fadd19e038f4fb7d799b09c301bcccab356486
Amit Assaraf <amita11@cs.huji.ac.il>
2013-06-26 14:56:53 +0300

    First Code

---
+++ WalkAMile/gen/com/meet/walkamile/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.meet.walkamile;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ WalkAMile/gen/in/wptrafficanalyzer/locationgooglemapv2demo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package in.wptrafficanalyzer.locationgooglemapv2demo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
