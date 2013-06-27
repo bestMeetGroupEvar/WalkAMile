7d913d8982a199409da3fb2101367310edd55a7b
Abdallah Amleh <abdallah11@cs.huji.ac.il>
2013-06-27 16:33:57 +0300

    added stuff to Roure and added RouteSet

--- WalkAMile/src/com/meet/walkamile/Route.java
+++ WalkAMile/src/com/meet/walkamile/Route.java
@@ -1,43 +1,56 @@
 package com.meet.walkamile;
 
 import java.util.ArrayList;
-
-import com.google.android.gms.maps.model.PolylineOptions;
+//TODO: add back in
+//import com.google.android.gms.maps.model.PolylineOptions;
 
 import android.location.Location;
 
 public class Route {
-	
-	 
-		//fileds
-	 ArrayList<Location> routes = new ArrayList<Location>();
-	 boolean[] hasBeen;
-	 
-	 public Route(ArrayList<Location> locs) {
-		 this.routes = locs;
-		 hasBeen = new boolean[routes.size()];
-	 }
-	 
-	 	//check off method
-	 public boolean checkOff(Location userPlace)	{
-		 for (int i = 0 ; i < routes.size(); i++) {
-			 if (routes.get(i).equals(userPlace) && hasBeen[i-1] == true) {
-				 hasBeen[i] = true;
-				 return true;
-			 } else if (hasBeen[i-1] == false) {
-				 return false;
-			 }
-		 }
-		 return false;
+
+	// fields
+	ArrayList<Location> routes = new ArrayList<Location>();
+	boolean[] hasBeen;
+	boolean routeFinished = false;
+
+	public Route(ArrayList<Location> locs) {
+		this.routes = locs;
+		hasBeen = new boolean[routes.size()];
 	}
-	 
+
+	// check off method
+	public boolean checkOff(Location userPlace) {
+		for (int i = 0; i < routes.size(); i++) {
+			if (i == routes.size() - 1) {
+				if (routes.get(routes.size()).equals(userPlace)
+						&& hasBeen[routes.size() - 1] == true) {
+					routeFinished = true;
+				}
+			}
+			if (routes.get(i).equals(userPlace) && hasBeen[i - 1] == true) {
+				hasBeen[i] = true;
+				return true;
+			} else if (hasBeen[i - 1] == false) {
+				return false;
+			}
+
+		}
+
+		return false;
+	}
+
 	public void drawRoute() {
-		PolylineOptions rectOptions = new PolylineOptions();
-		
+		// TODO: add back in
+		// PolylineOptions rectOptions = new PolylineOptions();
+
 	}
 
 	public ArrayList<Location> getRoutes() {
 		return routes;
+	}
+
+	public boolean isRouteFinished() {
+		return routeFinished;
 	}
 
 	public void setRoutes(ArrayList<Location> routes) {
